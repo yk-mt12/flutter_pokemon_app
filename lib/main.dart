2193233383e6084ff449c7f89b16fa21095cdcf9
yk-mt12 <agvksl543@gmail.com>
2022-10-31 18:48:07 +0900

    Added ポケモンを1体画面に表示した

--- lib/main.dart
+++ lib/main.dart
@@ -33,10 +33,37 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image.network(
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"),
+        body: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Stack(children: [
+        Container(
+          padding: const EdgeInsets.all(32),
+          child: Image.network(
+              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+              width: 100,
+              height: 100),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text(
+            'No.25',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ]),
+      const Text(
+        'pikachu',
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
       ),
-    );
+      const Chip(
+        label: Text('electric'),
+        backgroundColor: Colors.yellow,
+      ),
+//       )))
+    ])));
   }
 }
